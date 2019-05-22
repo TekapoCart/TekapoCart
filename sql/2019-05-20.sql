0f5675505af5497581f327beba3ac519a9bb32c8
TekapoCart <hi@tekapo.io>
2019-05-22 16:39:52 +0800

    - 新增 CSS component socialfollow.scss - Tune sticker 手機版的 top, left style - 多商店: 補上缺少的 meta_lang

--- sql/2019-05-20.sql
+++ sql/2019-05-20.sql
@@ -1,1 +1,15 @@
 INSERT INTO `ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `sort`) VALUES (NULL, 'displayHomeAfter', '首頁 主內容下方滿版', '', '1', '21');
+
+
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('8', '2', '1', '特價商品', '限時限量折扣專區', '', 'prices-drop');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('8', '3', '1', '特價商品', '限時限量折扣專區', '', 'prices-drop');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('8', '4', '1', '特價商品', '限時限量折扣專區', '', 'prices-drop');
+
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('6', '2', '1', '新品上架', '質感新鮮品', '', 'new-products');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('6', '3', '1', '新品上架', '質感新鮮品', '', 'new-products');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('6', '4', '1', '新品上架', '質感新鮮品', '', 'new-products');
+
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('51', '2', '1', '瀏覽紀錄', '', '', 'viewed-products');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('51', '3', '1', '瀏覽紀錄', '', '', 'viewed-products');
+INSERT INTO `ps_meta_lang` (`id_meta`, `id_shop`, `id_lang`, `title`, `description`, `keywords`, `url_rewrite`) VALUES ('51', '4', '1', '瀏覽紀錄', '', '', 'viewed-products');
+
