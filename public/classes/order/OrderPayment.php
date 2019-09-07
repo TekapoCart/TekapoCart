25552d282f9c813ea2d96b2e1029646c0193ce06
TekapoCart <hi@tekapo.io>
2019-09-07 21:06:14 +0800

    order_reference size 9 改成 15

--- public/classes/order/OrderPayment.php
+++ public/classes/order/OrderPayment.php
@@ -44,6 +44,7 @@ class OrderPaymentCore extends ObjectModel
         'table' => 'order_payment',
         'primary' => 'id_order_payment',
         'fields' => array(
+            // suzy: order_reference size 9 改成 15
             'order_reference' => array('type' => self::TYPE_STRING, 'validate' => 'isAnything', 'size' => 9),
             'id_currency' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
             'amount' => array('type' => self::TYPE_FLOAT, 'validate' => 'isNegativePrice', 'required' => true),
