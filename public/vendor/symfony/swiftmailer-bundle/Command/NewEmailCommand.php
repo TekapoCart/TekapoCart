5276adbe19b28ee71e594c5da06dc903761a48d0
TekapoCart <hi@tekapo.io>
2020-01-19 23:39:48 +0800

    - Installing symfony/http-client-contracts (v1.1.8) - Installing symfony/http-client (v4.4.2) - Installing symfony/polyfill-php72 (v1.13.1) - Installing symfony/polyfill-intl-idn (v1.13.1) - Installing symfony/service-contracts (v1.1.8) - Installing symfony/polyfill-iconv (v1.13.1) - Installing symfony/mime (v4.4.2) - Updating symfony/swiftmailer-bundle (v2.6.7 => v3.3.1)

--- public/vendor/symfony/swiftmailer-bundle/Command/NewEmailCommand.php
+++ public/vendor/symfony/swiftmailer-bundle/Command/NewEmailCommand.php
@@ -11,7 +11,6 @@
 
 namespace Symfony\Bundle\SwiftmailerBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,8 +21,10 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  *
  * @author Gusakov Nikita <dev@nkt.me>
  */
-class NewEmailCommand extends ContainerAwareCommand
+class NewEmailCommand extends AbstractSwiftMailerCommand
 {
+    protected static $defaultName = 'swiftmailer:email:send';
+
     /** @var SymfonyStyle */
     private $io;
 
@@ -33,7 +34,7 @@ class NewEmailCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('swiftmailer:email:send')
+            ->setName(static::$defaultName) // BC with 2.7
             ->setDescription('Send simple email message')
             ->addOption('from', null, InputOption::VALUE_REQUIRED, 'The from address of the message')
             ->addOption('to', null, InputOption::VALUE_REQUIRED, 'The to address of the message')
@@ -43,7 +44,8 @@ class NewEmailCommand extends ContainerAwareCommand
             ->addOption('content-type', null, InputOption::VALUE_REQUIRED, 'The body content type of the message', 'text/html')
             ->addOption('charset', null, InputOption::VALUE_REQUIRED, 'The body charset of the message', 'UTF8')
             ->addOption('body-source', null, InputOption::VALUE_REQUIRED, 'The source where body come from [stdin|file]', 'stdin')
-            ->setHelp(<<<EOF
+            ->setHelp(
+                <<<EOF
 The <info>%command.name%</info> command creates and sends a simple email message.
 
 <info>php %command.full_name% --mailer=custom_mailer --content-type=text/xml</info>
@@ -65,7 +67,7 @@ EOF
     }
 
     /**
-     * {@inheritdoc}
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -78,7 +80,7 @@ EOF
             case 'file':
                 $filename = $input->getOption('body');
                 $content = file_get_contents($filename);
-                if ($content === false) {
+                if (false === $content) {
                     throw new \Exception(sprintf('Could not get contents from "%s".', $filename));
                 }
                 $input->setOption('body', $content);
@@ -94,6 +96,8 @@ EOF
         $sentMessages = $mailer->send($message);
 
         $this->io->success(sprintf('%s emails were successfully sent.', $sentMessages));
+
+        return 0;
     }
 
     /**
@@ -102,7 +106,7 @@ EOF
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         foreach ($input->getOptions() as $option => $value) {
-            if ($value === null) {
+            if (null === $value) {
                 $input->setOption($option, $this->io->ask(sprintf('%s', ucfirst($option))));
             }
         }
