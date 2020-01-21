7a9ce983c9e9411370fd12775abfd014a50cebe1
TekapoCart <hi@tekapo.io>
2020-01-22 02:07:45 +0800

    ezship, ecpay_cvs 綠界711超商, ecpay_tcat 綠界黑貓宅配

--- public/modules/ezship_hd/translations/jp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{ezship_hd}prestashop>ezship_hd_550e252265d38a4f61179368159fb2ba'] = 'ezShip Home delivery';
-$_MODULE['<{ezship_hd}prestashop>ezship_hd_ad0322901b80cf443ba23c6544946ef9'] = '2-3 days';
-$_MODULE['<{ezship_hd}prestashop>ezship_hd_6cefaa978ccec960693d10cefeb2c2bf'] = 'Home delivery';
-$_MODULE['<{ezship_hd}prestashop>ezship_hd_2084d4d72acfc24b948650eec4baf1c4'] = 'Receiver name over limit';
-$_MODULE['<{ezship_hd}prestashop>ezship_hd_d8879c62708a902f3274d4277bcacd90'] = 'Invalid mobile phone format';
--- public/modules/ezship_hd/views/templates/hook/tab_order.tpl
+++
@@ -1,6 +0,0 @@
-<li>
-    <a href="#ezship_hd">
-        <i class="icon-truck"></i>
-        {$tab_title} <span class="badge" style="visibility: hidden;">0</span>
-    </a>
-</li>
