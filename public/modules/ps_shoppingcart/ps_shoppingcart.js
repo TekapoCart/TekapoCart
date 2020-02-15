11057ee7dbb0735c55946170ceb8012d9d2afc78
TekapoCart <hi@tekapo.io>
2020-02-16 02:52:43 +0800

    取消用 ajax 取得購物車數量

--- public/modules/ps_shoppingcart/ps_shoppingcart.js
+++ public/modules/ps_shoppingcart/ps_shoppingcart.js
@@ -30,14 +30,14 @@ $(document).ready(function () {
   };
 
   // suzy: 2020-02-09 為 varnish 做準備
-  var refreshURL = $('.blockcart').data('refresh-url');
-  var requestData = {};
-  $.post(refreshURL, requestData).then(function (resp) {
-    $('.blockcart').replaceWith($(resp.preview).find('.blockcart'));
-    if (resp.modal) {
-      showModal(resp.modal);
-    }
-  })
+  // var refreshURL = $('.blockcart').data('refresh-url');
+  // var requestData = {};
+  // $.post(refreshURL, requestData).then(function (resp) {
+  //   $('.blockcart').replaceWith($(resp.preview).find('.blockcart'));
+  //   if (resp.modal) {
+  //     showModal(resp.modal);
+  //   }
+  // })
 
   prestashop.on(
     'updateCart',
