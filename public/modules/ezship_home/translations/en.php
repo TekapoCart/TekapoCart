1d82b902f6e5334f52253defd63513b24b3f761a
TekapoCart <hi@tekapo.io>
2020-02-19 00:07:21 +0800

    typo

--- public/modules/ezship_home/translations/en.php
+++ public/modules/ezship_home/translations/en.php
@@ -9,17 +9,17 @@ $_MODULE['<{ezship_home}prestashop>ezship_home_2084d4d72acfc24b948650eec4baf1c4'
 $_MODULE['<{ezship_home}prestashop>ezship_home_d8879c62708a902f3274d4277bcacd90'] = 'Invalid mobile phone format';
 $_MODULE['<{ezship_home}prestashop>ezship_home_4707166baa30e68a936b62bc2e96f5ef'] = 'Invalid delivery address';
 
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_4d1c2dd237da2d9c3dba6abfeb5b7639'] = 'Pickup Info';
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_70e5f2f79f0df6677abfa15cadb67a84'] = 'Receiver Name';
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_a01a6be1f61438ab400738791a61a8de'] = 'Receiver Phone';
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_628ea294212a46ece79f0236f20133fd'] = 'Receiver City';
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_f4fc962d5586a24d453a3b0f26e6faea'] = 'Receiver Postcode';
-$_MODULE['<{ecpay_home}prestashop>display_order_confirmation_c5b8c2379912db4b6ace799089d2c65c'] = 'Receiver Address';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_4d1c2dd237da2d9c3dba6abfeb5b7639'] = 'Pickup Info';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_70e5f2f79f0df6677abfa15cadb67a84'] = 'Receiver Name';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_a01a6be1f61438ab400738791a61a8de'] = 'Receiver Phone';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_628ea294212a46ece79f0236f20133fd'] = 'Receiver City';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_f4fc962d5586a24d453a3b0f26e6faea'] = 'Receiver Postcode';
+$_MODULE['<{ezship_home}prestashop>display_order_confirmation_c5b8c2379912db4b6ace799089d2c65c'] = 'Receiver Address';
 
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_4d1c2dd237da2d9c3dba6abfeb5b7639'] = 'Pickup Info';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_70e5f2f79f0df6677abfa15cadb67a84'] = 'Receiver Name';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_a01a6be1f61438ab400738791a61a8de'] = 'Receiver Phone';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_628ea294212a46ece79f0236f20133fd'] = 'Receiver City';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_f4fc962d5586a24d453a3b0f26e6faea'] = 'Receiver Postcode';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_c5b8c2379912db4b6ace799089d2c65c'] = 'Receiver Address';
-$_MODULE['<{ecpay_home}prestashop>display_order_detail_1165eb193193f8ee509397d92f1e47ee'] = 'Shipping Log';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_4d1c2dd237da2d9c3dba6abfeb5b7639'] = 'Pickup Info';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_70e5f2f79f0df6677abfa15cadb67a84'] = 'Receiver Name';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_a01a6be1f61438ab400738791a61a8de'] = 'Receiver Phone';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_628ea294212a46ece79f0236f20133fd'] = 'Receiver City';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_f4fc962d5586a24d453a3b0f26e6faea'] = 'Receiver Postcode';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_c5b8c2379912db4b6ace799089d2c65c'] = 'Receiver Address';
+$_MODULE['<{ezship_home}prestashop>display_order_detail_1165eb193193f8ee509397d92f1e47ee'] = 'Shipping Log';
