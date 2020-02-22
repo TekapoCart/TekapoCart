b9075fa1fccae6fd5d6792a654c76f8762b0a7ea
TekapoCart <hi@tekapo.io>
2020-02-22 21:20:05 +0800

    rewrite index.php?controller=order

--- public/modules/ecpay/controllers/front/validation.php
+++ public/modules/ecpay/controllers/front/validation.php
@@ -36,7 +36,7 @@ class EcpayValidationModuleFrontController extends ModuleFrontController
     {
         $cart = $this->context->cart;
         if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active) {
-            Tools::redirect('index.php?controller=order&step=1');
+            Tools::redirect('order?step=1');
         }
 
         // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
