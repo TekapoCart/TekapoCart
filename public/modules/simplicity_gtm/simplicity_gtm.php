4ac2b33212fdece5031a4911aedb2e02a7e6286d
TekapoCart <hi@tekapo.io>
2020-02-25 14:10:36 +0800

    GTM bottom 是 OK 的

--- public/modules/simplicity_gtm/simplicity_gtm.php
+++ public/modules/simplicity_gtm/simplicity_gtm.php
@@ -66,7 +66,6 @@ class Simplicity_Gtm extends Module
             'backOfficeHeader',
             'displayAdminOrderTabOrder',
             'displayAdminOrderContentOrder',
-
         );
 
         $installed = (parent::install() &&
@@ -338,7 +337,7 @@ class Simplicity_Gtm extends Module
         $this->context->controller->registerJavascript(
             'modules-' . $this->name,
             'modules/' . $this->name . '/simplicity_gtm.js',
-            array('position' => 'head', 'priority' => 150)
+            array('position' => 'bottom', 'priority' => 150)
         );
 
         return $this->fetch('module:' . $this->name . '/hook-header.tpl');
