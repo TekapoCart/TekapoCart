845d986cef343ddfb2b0ddf509cab6f533c61bea
TekapoCart <hi@tekapo.io>
2020-03-07 22:40:17 +0800

    pwa 測試

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ const networkFirstPaths = [
 const avoidCachingPaths = [
     /* Add an array of regex of paths that shouldn't be cached */
     // Example: /\/api\/.*/
-    /\/tekapo.*/,
+    /\/tekapo\/.*/,
     /\/order.*/
 ];
 
