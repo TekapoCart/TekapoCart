ef9dfb6e17edeba47d32efd86a959c0dcc97a9d3
TekapoCart <hi@tekapo.io>
2020-05-22 23:05:46 +0800

    文字修改

---
+++ public/modules/ps_themecusto/translations/tw.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ps_themecusto}prestashop>index_339f7a2e087a159e6a31501649027a77'] = '首頁';
+$_MODULE['<{ps_themecusto}prestashop>index_bc4327368b4abb06c1ea90fe0ddd36d6'] = '分類頁';
+$_MODULE['<{ps_themecusto}prestashop>index_eb763a2e313718f33f55e1f0d84df6ee'] = '商品頁';
+
