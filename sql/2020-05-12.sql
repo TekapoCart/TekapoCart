2bae7f7ed8d5fbd261da13ab186a2d2e4e645aad
TekapoCart <hi@tekapo.io>
2020-05-23 00:04:19 +0800

    sql 更新

--- sql/2020-05-12.sql
+++ sql/2020-05-12.sql
@@ -1,5 +1,7 @@
-INSERT IGNORE INTO `ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `sort`) VALUES (NULL, 'displayExpressCheckout', 'ExpressCheckout', '', '1', '0');
-INSERT IGNORE INTO `ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `sort`) VALUES (NULL, 'displayCheckoutSummaryBottom', 'displayCheckoutSummaryBottom', '', '1', '0');
+-- new hooks for loyalty module
+INSERT IGNORE INTO `ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `sort`) VALUES (456, 'displayExpressCheckout', 'ExpressCheckout', '', '1', '0');
+INSERT IGNORE INTO `ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `sort`) VALUES (457, 'displayCheckoutSummaryBottom', 'displayCheckoutSummaryBottom', '', '1', '0');
 
+-- ps_googleanalytics upgrade
 INSERT IGNORE INTO `ps_hook_module` (`id_module`, `id_shop`, `id_hook`, `position`) VALUES ('69', '1', '35', '2'), ('69', '2', '35', '2'), ('69', '3', '35', '2'), ('69', '4', '35', '2');
 INSERT IGNORE INTO `ps_hook_module` (`id_module`, `id_shop`, `id_hook`, `position`) VALUES ('69', '1', '13', '10'), ('69', '2', '13', '10'), ('69', '3', '13', '10'), ('69', '4', '13', '10');
