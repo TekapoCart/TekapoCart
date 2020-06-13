3d937352423595fcd667c48889837a6bdca953dc
TekapoCart <hi@tekapo.io>
2020-06-13 21:01:09 +0800

    部落格

--- public/classes/CMSCategory.php
+++ public/classes/CMSCategory.php
@@ -209,9 +209,18 @@ class CMSCategoryCore extends ObjectModel
             ];
         }
 
-        $subcats = $this->getSubCategories($id_lang, $active);
-        if ($subcats && count($subcats)) {
-            foreach ($subcats as &$subcat) {
+        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+		SELECT c.*, cl.id_lang, cl.name, cl.description, cl.link_rewrite, cl.meta_title, cl.meta_keywords, cl.meta_description
+		FROM `' . _DB_PREFIX_ . 'cms_category` c
+		' . Shop::addSqlAssociation('cms_category', 'c') . '
+		LEFT JOIN `' . _DB_PREFIX_ . 'cms_category_lang` cl ON (c.`id_cms_category` = cl.`id_cms_category` AND `id_lang` = ' . (int) $id_lang . ')
+		WHERE `id_parent` = ' . (int) $this->id . '
+		' . ($active ? 'AND `active` = 1' : '') . '
+		GROUP BY c.`id_cms_category`
+		ORDER BY `position` ASC');
+
+        if ($result && count($result)) {
+            foreach ($result as &$subcat) {
                 $categ = new CMSCategory($subcat['id_cms_category'], $id_lang);
                 $pairs[$categ->id_cms_category] = [
                     'id_cms_category' => $categ->id_cms_category,
