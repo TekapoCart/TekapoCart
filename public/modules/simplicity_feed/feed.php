df61248eaa9bf92afc418cb4c1f14be4f2b6734e
TekapoCart <hi@tekapo.io>
2020-06-14 19:52:12 +0800

    產品目錄

--- public/modules/simplicity_feed/feed.php
+++ public/modules/simplicity_feed/feed.php
@@ -1,4 +1,8 @@
 <?php
+// suzy: 2019-09-07 不讓 Browser Keep Cache
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 include_once('../../config/config.inc.php');
 include_once('../../init.php');
@@ -9,6 +13,16 @@ if (strlen(Configuration::get('simplicity_feed_secret')) > 0 && Tools::getValue(
     $m = new Simplicity_Feed();
 
     $m->generateFeed();
+
+} else {
+
+
+    // suzy: 2019-09-07 不讓 Browser Keep Cache
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
+    Tools::redirect('index.php?controller=pagenotfound');
 
 }
 
