250b3649d914d473689e6f6e078efb83e40d3008
TekapoCart <hi@tekapo.io>
2020-06-23 00:19:53 +0800

    cron ip 判斷

--- public/modules/ps_facetedsearch/ps_facetedsearch-attribute-indexer.php
+++ public/modules/ps_facetedsearch/ps_facetedsearch-attribute-indexer.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../config/config.inc.php';
 require_once __DIR__ . '/ps_facetedsearch.php';
 
 // suzy: 2020-03-06 改寫 cron 檢查條件
-if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1') {
+if (Tools::isLocalIp($_SERVER['REMOTE_ADDR'])) {
     if (substr(Tools::encrypt('ps_facetedsearch/index'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('ps_facetedsearch')) {
         die('Bad token');
     }
